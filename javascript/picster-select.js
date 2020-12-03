1cfef1857deaf2d4336de45d633194b6aed02017
James Cheung <cheungtszhim@gmail.com>
2020-12-03 02:03:51 +0100

    Change trigger to "O", responding to general expressions rather than just djster expressions

--- javascript/picster-select.js
+++ javascript/picster-select.js
@@ -1951,9 +1951,6 @@ function anything()
 				}
 			}
 			break;
-			case 68 : //create Djster Notation
-			addShapeFromExpression();
-			break;
 			case 69 :  //edit
 			if (foundobjects.contains("0") && item != -1) edit.parse(foundobjects.get(item)[foundobjects.get(item).length - 1]);
 			outlet(3, "edit");
@@ -2065,6 +2062,9 @@ function anything()
 			break;
 			case 77 :
 			preference = "measure";
+			break;
+			case 79 :
+			addShapeFromExpression();
 			break;
 			case 83 :
 			preference = "staff";
@@ -2534,40 +2534,56 @@ function renderDrawSocket(d)
 }
 
 function addShapeFromExpression() {
-	post("d pressed\n");
+	post("o pressed\n");
 	if (foundobjects.contains("0") && item != -1) {
-		post("object found\n");
-		edit.parse(foundobjects.get(item)[foundobjects.get(item).length - 1]);
-		var timeStamp = Date.now();
-		var outputPicster = JSON.parse('{"picster-element":[{"key":"svg","val":{"new":"g","id":"Picster-Element_'+timeStamp+'","child":[],"transform":"matrix(1,0,0,1,0,0)"}},{"key":"extras","val":{"bounds":[-1,-1,-1,-1]}}]}');
+		//post("object found\n");
+		edit.parse(foundobjects.get(item)[foundobjects.get(item).length-1]);
+		if (edit.contains("picster-element[2]::key") && edit.get("picster-element[2]::key") == "expression") {
+			//post("Picster element contains expressions\n");
+			var i = 0;
+			while (edit.contains("picster-element[2]::val["+i+"]::editor")) { // loop until all expressions are done
+				post("Processing the "+i+"th expression\n");
+				switch (edit.get("picster-element[2]::val["+i+"]::editor")) { // type of expression
+					case "djster" :
+					//picster group template
+					var timeStamp = Date.now();
+					var outputPicster = JSON.parse('{"picster-element":[{"key":"svg","val":{"new":"g","id":"Picster-Element_'+timeStamp+'","child":[],"transform":"matrix(1,0,0,1,0,0)"}},{"key":"extras","val":{"bounds":[-1,-1,-1,-1]}}]}');
+					//parse djster attributes from Dict edit
+					var djsterAttributes = JSON.parse(edit.stringify())["picster-element"][2]["val"][i]["value"];
 
-    if (edit.contains("picster-element[2]::val[0]::message") && edit.get("picster-element[2]::val[0]::message") == "djster") {
-			var djsterAttributes = JSON.parse(edit.stringify())["picster-element"][2]["val"][0]["value"];
-			//post("temp == "+JSON.stringify(temp)+"\n");
-			if ("scale" in djsterAttributes) {
-				timeStamp++;
-				var element = JSON.parse('{"new":"text","x":140,"y":30,"font-family":"Aloisen New","font-size":18,"style":{"fill":"rgb(0,0,0)","fill-opacity":1},"transform":"matrix(1,0,0,1,0,0)"}');
-				element.id = "Picster-Element_"+timeStamp;
-				element.child = djsterAttributes.scale;
+					if ("scale" in djsterAttributes) {
+						timeStamp++;
+						var element = JSON.parse('{"new":"text","x":140,"y":30,"font-family":"Aloisen New","font-size":18,"style":{"fill":"rgb(0,0,0)","fill-opacity":1},"transform":"matrix(1,0,0,1,0,0)"}');
+						element.id = "Picster-Element_"+timeStamp;
+						element.child = djsterAttributes.scale;
 
-				outputPicster["picster-element"][0]["val"]["child"].push(element);
+						outputPicster["picster-element"][0]["val"]["child"].push(element);
+					}
+
+					/*if ("meter" in djsterAttributes) {
+						timeStamp++;
+						var element = JSON.parse('{"new":"g","id":"Picster-Element_'+timeStamp+'","child":[],"transform":"matrix(1,0,0,1,0,0)"}');
+						var l = djsterAttributes.meter.length;
+
+
+					}
+					*/
+
+					//draw new picster element
+					edit.parse(JSON.stringify(outputPicster));
+				  action = "addShape";
+				  outlet(3, "bang");
+					break;
+
+					default :
+					post("Picster expression of type \""+edit.get("picster-element[2]::val["+i+"]::editor")+"\" is found.\n");
+					break;
+				}
+				i++;
 			}
-
-			if ("meter" in djsterAttributes) {
-				timeStamp++;
-				var element = JSON.parse('{"new":"g","id":"Picster-Element_'+timeStamp+'","child":[],"transform":"matrix(1,0,0,1,0,0)"}');
-				var l = djsterAttributes.meter.length;
-
-
-			}
-
-
-
 		}
+
   }
-	edit.parse(JSON.stringify(outputPicster));
-  action = "addShape";
-  outlet(3, "bang");
 }
 
 function htmlEntities(str)
