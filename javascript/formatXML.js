372e7b64e654dfefda9b2aa2ca5ba9a259e3fea0
computermusik <georghajdu@mac.com>
2021-06-29 16:02:36 +0200

    Create formatXML.js

---
+++ javascript/formatXML.js
@@ -0,0 +1,71 @@
+var mystring = "<jmslscoredoc><score ScoreTitleFontScaler=\"4.\" NAME=\"JMSLMaxScore-30\" HEIGHT=\"220\" FirstSystemIndent=\"0.\" StaffNumbersVisible=\"true\" ClefsVisible=\"true\" RightMargin=\"20.\" TextFontScaler=\"1.5\" TimesigFontName=\"Serif\" InstrumentNamesVisible=\"false\" TimeSignaturesVisible=\"true\" MeasureNumberOffset=\"1\" TopMarginOfFirstPage=\"60.\" TimesigFontScaler=\"2.5\" ScoreSubtitleFontName=\"Serif\" STAFFS=\"2\" useLegacyMultiTrackOrientation=\"true\" MeasureNumbersVisible=\"true\" ScoreTitleVisible=\"false\" TopMargin=\"15.\" ScoreSubtitleFontScaler=\"2.\" TextFontName=\"SansSerif\" WIDTH=\"320\" MeasureNumberFontName=\"SansSerif\" TempoVisible=\"true\" KeySignaturesVisible=\"true\" LeftMargin=\"20.\" DrawAllMeasureNumbers=\"false\" MeasureNumberFontScaler=\"1.5\" ScoreTitleFontName=\"Serif\" NUMTRACKSPERSTAFF=\"2\" SectionBracketsVisible=\"true\" CourtesyClefsVisible=\"false\" BottomMargin=\"15.\"><ScoreAnnotation CLASSNAME=\"com.softsynth.jmsl.score.ScoreAnnotation\" Annotation=\"324.3oc4PtrSCCCEDccxWgkWmD4HwCU1QU2vBJU7PHAhENI1QlZ6ajsSATU+2wOZSCeCrwV9bGOZl697L7NlwJ.MFcCBWWQvEd1fAF.iyioxv.R.1Ho5san8rIhSnXunEt.nlPpRP12NNniP7sFg2g.VMZEsS7kF5tQCEmbwIYSSd16oEsl8E5QPQ0QEM8sfDLg4uiHUKVbUwrqKJP9bi9Hnj+WkjhYGShrL2qF5PL0Ge+F.pXspt7HYkOdoEvSCzVgteI0lZd00QINJmWRBjr84YYXIqqmYjBMydx4fpejwegWw3zQoCGwJQqw2tXDvp6K0flklP69bz5Nsgyvs9USTU27+GiVoDfsiCm0FiTuARLe+KPk0wJ68sowbm15LqopTfPIqDA3nhocgIa.qHz5G3b+NHZcd1g7yEt9+QgyOj+a2Lbcb\"/><orchestra CLASSNAME=\"com.softsynth.jmsl.score.Orchestra\"><jmslscoreinstrument MixerClassName=\"com.softsynth.jmsl.NullMixer\" InsIndex=\"0\" Transposition=\"0.\" CLASSNAME=\"com.algomusic.max.MaxScoreInstrument\" EditEnabled=\"true\" Name=\"MaxScoreIns-0\"><dim lowlimit=\"0.\" index=\"4\" highlimit=\"3.\" defaultvalue=\"0.\" name=\"EventFlag\"/><dim lowlimit=\"-1.\" index=\"5\" highlimit=\"127.\" defaultvalue=\"-1.\" name=\"originalPitch\"/><dim lowlimit=\"-1.\" index=\"6\" highlimit=\"10000.\" defaultvalue=\"-1.\" name=\"index\"/></jmslscoreinstrument><jmslscoreinstrument MixerClassName=\"com.softsynth.jmsl.NullMixer\" InsIndex=\"1\" Transposition=\"0.\" CLASSNAME=\"com.algomusic.max.MaxScoreInstrument\" EditEnabled=\"true\" Name=\"MaxScoreIns-1\"><dim lowlimit=\"0.\" index=\"4\" highlimit=\"3.\" defaultvalue=\"0.\" name=\"EventFlag\"/><dim lowlimit=\"-1.\" index=\"5\" highlimit=\"127.\" defaultvalue=\"-1.\" name=\"originalPitch\"/><dim lowlimit=\"-1.\" index=\"6\" highlimit=\"10000.\" defaultvalue=\"-1.\" name=\"index\"/></jmslscoreinstrument></orchestra><mixerpanelsettings><panamppair PAN=\"0.5\" AMP=\"0.5\" FADERINDEX=\"0\"/><panamppair PAN=\"0.5\" AMP=\"0.5\" FADERINDEX=\"1\"/></mixerpanelsettings><staffspacing BELOW=\"72.\" INDEX=\"0\" ABOVE=\"72.\"/><staffspacing BELOW=\"72.\" INDEX=\"1\" ABOVE=\"72.\"/><scoresection NAME=\"A\" START=\"0\" END=\"0\"/><scoresection NAME=\"B\" START=\"0\" END=\"0\"/><scoresection NAME=\"C\" START=\"0\" END=\"0\"/><scoresection NAME=\"D\" START=\"0\" END=\"0\"/><scoresection NAME=\"E\" START=\"0\" END=\"0\"/><scoresection NAME=\"F\" START=\"0\" END=\"0\"/><scoresection NAME=\"G\" START=\"0\" END=\"0\"/><scoresection NAME=\"H\" START=\"0\" END=\"0\"/><scoresection NAME=\"I\" START=\"0\" END=\"0\"/><scoresection NAME=\"J\" START=\"0\" END=\"0\"/><scoresection NAME=\"K\" START=\"0\" END=\"0\"/><scoresection NAME=\"L\" START=\"0\" END=\"0\"/><scoresection NAME=\"M\" START=\"0\" END=\"0\"/><scoresection NAME=\"N\" START=\"0\" END=\"0\"/><scoresection NAME=\"O\" START=\"0\" END=\"0\"/><scoresection NAME=\"P\" START=\"0\" END=\"0\"/><scoresection NAME=\"Q\" START=\"0\" END=\"0\"/><scoresection NAME=\"R\" START=\"0\" END=\"0\"/><scoresection NAME=\"S\" START=\"0\" END=\"0\"/><scoresection NAME=\"T\" START=\"0\" END=\"0\"/><scoresection NAME=\"U\" START=\"0\" END=\"0\"/><scoresection NAME=\"V\" START=\"0\" END=\"0\"/><scoresection NAME=\"W\" START=\"0\" END=\"0\"/><scoresection NAME=\"X\" START=\"0\" END=\"0\"/><scoresection NAME=\"Y\" START=\"0\" END=\"0\"/><scoresection NAME=\"Z\" START=\"0\" END=\"0\"/><measure TIMESIGSETBYHAND=\"false\" NUMREPEATS=\"1\" MEASURELEFTMARGIN=\"50.\" WIDTHSETBYHAND=\"false\" REPEATSTART=\"false\" MEASURETEXTX=\"48\" TEMPO=\"60.\" BARLINE=\"SINGLE\" WIDTH=\"561\" TIMESIG=\"4 4\" REPEATEND=\"false\" MEASURETEXTY=\"48\" TEMPOSETBYHAND=\"false\"><staff KEYSIGTYPE=\"0\" INDEX=\"0\" INSTRUMENTINDEX=\"0\" EXTENDEDLINESABOVE=\"0\" CLEF=\"0\" KEYSIGNUMACC=\"0\" INSINDEXSETBYHAND=\"false\" EXTENDEDLINESBELOW=\"0\" CLEFSETBYHAND=\"false\" KEYSIGSETBYHAND=\"false\"><track MultitrackRestAdjustmentY=\"0.\" INDEX=\"0\"><note LEDGERLINESVISIBLE=\"true\" TEXTOFFSETY=\"0\" STEMVISIBLE=\"true\" TUPLET=\"0\" LAYOUTSHIFTX=\"0.\" VELOCITY=\"0.5\" ACCPREF=\"0\" ISGRACENOTE=\"false\" MARK=\"0\" VISIBLE=\"true\" STEMINFOOVERRIDE=\"false\" DURATION=\"1.\" GLISSOUT=\"false\" DYN=\"0\" WEDGE=\"none\" NOTEHEAD=\"0\" OVERRIDELEVEL=\"-1\" DOTS=\"0\" HOLD=\"1.\" ACCVISPOLICY=\"0\" GRACENOTESEPARATIONSCALER=\"2.\" TEXTOFFSETX=\"0\" NOTEHEADVISIBLE=\"true\" NOTEDUR=\"2\" STEMINFO=\"2\" PITCH=\"72.\" TIEDOUT=\"false\" SLUROUT=\"false\" OTTAVA=\"none\" NOTEHEADSCALE=\"1.\" ISOVERRIDELEVEL=\"false\" ACCINFO=\"0\" BEAMEDOUT=\"false\" ALTENHARMONIC=\"false\"><dim index=\"4\" name=\"EventFlag\" value=\"0.\"/><dim index=\"5\" name=\"originalPitch\" value=\"-1.\"/><dim index=\"6\" name=\"index\" value=\"-1.\"/></note><note LEDGERLINESVISIBLE=\"true\" TEXTOFFSETY=\"0\" STEMVISIBLE=\"true\" TUPLET=\"0\" LAYOUTSHIFTX=\"0.\" VELOCITY=\"0.5\" ACCPREF=\"0\" ISGRACENOTE=\"false\" MARK=\"0\" VISIBLE=\"true\" STEMINFOOVERRIDE=\"false\" DURATION=\"1.\" GLISSOUT=\"false\" DYN=\"0\" WEDGE=\"none\" NOTEHEAD=\"0\" OVERRIDELEVEL=\"-1\" DOTS=\"0\" HOLD=\"1.\" ACCVISPOLICY=\"0\" GRACENOTESEPARATIONSCALER=\"2.\" TEXTOFFSETX=\"0\" NOTEHEADVISIBLE=\"true\" NOTEDUR=\"2\" STEMINFO=\"2\" PITCH=\"71.\" TIEDOUT=\"false\" SLUROUT=\"false\" OTTAVA=\"none\" NOTEHEADSCALE=\"1.\" ISOVERRIDELEVEL=\"false\" ACCINFO=\"0\" BEAMEDOUT=\"false\" ALTENHARMONIC=\"false\"><dim index=\"4\" name=\"EventFlag\" value=\"0.\"/><dim index=\"5\" name=\"originalPitch\" value=\"-1.\"/><dim index=\"6\" name=\"index\" value=\"-1.\"/></note><note LEDGERLINESVISIBLE=\"true\" TEXTOFFSETY=\"0\" STEMVISIBLE=\"true\" TUPLET=\"0\" LAYOUTSHIFTX=\"0.\" VELOCITY=\"0.5\" ACCPREF=\"0\" ISGRACENOTE=\"false\" MARK=\"0\" VISIBLE=\"true\" STEMINFOOVERRIDE=\"false\" DURATION=\"1.\" GLISSOUT=\"false\" DYN=\"0\" WEDGE=\"none\" NOTEHEAD=\"0\" OVERRIDELEVEL=\"-1\" DOTS=\"0\" HOLD=\"1.\" ACCVISPOLICY=\"0\" GRACENOTESEPARATIONSCALER=\"2.\" TEXTOFFSETX=\"0\" NOTEHEADVISIBLE=\"true\" NOTEDUR=\"2\" STEMINFO=\"1\" PITCH=\"67.\" TIEDOUT=\"false\" SLUROUT=\"false\" OTTAVA=\"none\" NOTEHEADSCALE=\"1.\" ISOVERRIDELEVEL=\"false\" ACCINFO=\"0\" BEAMEDOUT=\"false\" ALTENHARMONIC=\"false\"><dim index=\"4\" name=\"EventFlag\" value=\"0.\"/><dim index=\"5\" name=\"originalPitch\" value=\"-1.\"/><dim index=\"6\" name=\"index\" value=\"-1.\"/></note></track><track MultitrackRestAdjustmentY=\"0.\" INDEX=\"1\"/></staff><staff KEYSIGTYPE=\"0\" INDEX=\"1\" INSTRUMENTINDEX=\"1\" EXTENDEDLINESABOVE=\"0\" CLEF=\"0\" KEYSIGNUMACC=\"0\" INSINDEXSETBYHAND=\"false\" EXTENDEDLINESBELOW=\"0\" CLEFSETBYHAND=\"false\" KEYSIGSETBYHAND=\"false\"><track MultitrackRestAdjustmentY=\"0.\" INDEX=\"0\"><note LEDGERLINESVISIBLE=\"true\" TEXTOFFSETY=\"0\" STEMVISIBLE=\"true\" TUPLET=\"0\" LAYOUTSHIFTX=\"0.\" VELOCITY=\"0.5\" ACCPREF=\"0\" ISGRACENOTE=\"false\" MARK=\"0\" VISIBLE=\"true\" STEMINFOOVERRIDE=\"false\" DURATION=\"1.\" GLISSOUT=\"false\" DYN=\"0\" WEDGE=\"none\" NOTEHEAD=\"0\" OVERRIDELEVEL=\"-1\" DOTS=\"0\" HOLD=\"1.\" ACCVISPOLICY=\"0\" GRACENOTESEPARATIONSCALER=\"2.\" TEXTOFFSETX=\"0\" NOTEHEADVISIBLE=\"true\" NOTEDUR=\"2\" STEMINFO=\"1\" PITCH=\"69.\" TIEDOUT=\"false\" SLUROUT=\"false\" OTTAVA=\"none\" NOTEHEADSCALE=\"1.\" ISOVERRIDELEVEL=\"false\" ACCINFO=\"0\" BEAMEDOUT=\"false\" ALTENHARMONIC=\"false\"><dim index=\"4\" name=\"EventFlag\" value=\"0.\"/><dim index=\"5\" name=\"originalPitch\" value=\"-1.\"/><dim index=\"6\" name=\"index\" value=\"-1.\"/></note><note LEDGERLINESVISIBLE=\"true\" TEXTOFFSETY=\"0\" STEMVISIBLE=\"true\" TUPLET=\"0\" LAYOUTSHIFTX=\"0.\" VELOCITY=\"0.5\" ACCPREF=\"0\" ISGRACENOTE=\"false\" MARK=\"0\" VISIBLE=\"true\" STEMINFOOVERRIDE=\"false\" DURATION=\"1.\" GLISSOUT=\"false\" DYN=\"0\" WEDGE=\"none\" NOTEHEAD=\"0\" OVERRIDELEVEL=\"-1\" DOTS=\"0\" HOLD=\"1.\" ACCVISPOLICY=\"0\" GRACENOTESEPARATIONSCALER=\"2.\" TEXTOFFSETX=\"0\" NOTEHEADVISIBLE=\"true\" NOTEDUR=\"2\" STEMINFO=\"2\" PITCH=\"71.\" TIEDOUT=\"false\" SLUROUT=\"false\" OTTAVA=\"none\" NOTEHEADSCALE=\"1.\" ISOVERRIDELEVEL=\"false\" ACCINFO=\"0\" BEAMEDOUT=\"false\" ALTENHARMONIC=\"false\"><dim index=\"4\" name=\"EventFlag\" value=\"0.\"/><dim index=\"5\" name=\"originalPitch\" value=\"-1.\"/><dim index=\"6\" name=\"index\" value=\"-1.\"/></note></track><track MultitrackRestAdjustmentY=\"0.\" INDEX=\"1\"/></staff></measure><measure TIMESIGSETBYHAND=\"false\" NUMREPEATS=\"1\" MEASURELEFTMARGIN=\"50.\" WIDTHSETBYHAND=\"false\" REPEATSTART=\"false\" MEASURETEXTX=\"48\" TEMPO=\"60.\" BARLINE=\"SINGLE\" WIDTH=\"450\" TIMESIG=\"4 4\" REPEATEND=\"false\" MEASURETEXTY=\"48\" TEMPOSETBYHAND=\"false\"><staff KEYSIGTYPE=\"0\" INDEX=\"0\" INSTRUMENTINDEX=\"0\" EXTENDEDLINESABOVE=\"0\" CLEF=\"0\" KEYSIGNUMACC=\"0\" INSINDEXSETBYHAND=\"false\" EXTENDEDLINESBELOW=\"0\" CLEFSETBYHAND=\"false\" KEYSIGSETBYHAND=\"false\"><track MultitrackRestAdjustmentY=\"0.\" INDEX=\"0\"/><track MultitrackRestAdjustmentY=\"0.\" INDEX=\"1\"/></staff><staff KEYSIGTYPE=\"0\" INDEX=\"1\" INSTRUMENTINDEX=\"1\" EXTENDEDLINESABOVE=\"0\" CLEF=\"0\" KEYSIGNUMACC=\"0\" INSINDEXSETBYHAND=\"false\" EXTENDEDLINESBELOW=\"0\" CLEFSETBYHAND=\"false\" KEYSIGSETBYHAND=\"false\"><track MultitrackRestAdjustmentY=\"0.\" INDEX=\"0\"/><track MultitrackRestAdjustmentY=\"0.\" INDEX=\"1\"/></staff></measure><scoreUserBean CLASSNAME=\"com.algomusic.max.MaxScoreRenderedMessageListener\"/></score></jmslscoredoc>"
+
+function bang()
+{
+	var arr = formatXml(mystring).split("\n");
+	outlet(0, "startdump", "dumpScore");
+	for (var i = 0; i < arr.length - 30; i++) outlet(0, arr[i]);
+	outlet(0, "enddump", "dumpScore");
+}
+
+function loadScore(path)
+{
+	var f = new File(path);
+	var a = "";
+ 	var str = "";
+	if (f.isopen) {
+	while ((a = f.readline()) != null) {
+		if (typeof a != "undefined") str += a; 
+		}
+	var arr = formatXml(str).split("\n");
+	outlet(0, "startdump", "dumpScore");
+	for (var i = 0; i < arr.length; i++) {
+	//post("str", arr[i], "\n");		
+		if (arr[i].length >= 7) outlet(0, arr[i]);
+	}
+	outlet(0, "enddump", "dumpScore");
+	}
+	f.close();
+}
+
+function formatXml(xml){
+    var out = "";
+    var tab = "";
+    var indent = 0;
+    var inClosingTag=false;
+    var dent=function(no){
+        out += "\n";
+        for(var i=0; i < no; i++)
+            out+=tab;
+    }
+
+
+    for (var i=0; i < xml.length; i++) {
+        var c = xml.charAt(i);
+        if(c=='<'){
+            // handle </
+            if(xml.charAt(i+1) == '/'){
+                inClosingTag = true;
+                dent(--indent);
+            }
+            out+=c;
+        }else if(c=='>'){
+            out+=c;
+            // handle />
+            if(xml.charAt(i-1) == '/'){
+                out+="\n";
+                //dent(--indent)
+            }else{
+              if(!inClosingTag)
+                dent(++indent);
+              else{
+                out+="\n";
+                inClosingTag=false;
+              }
+            }
+        }else{
+          out+=c;
+        }
+    }
+    return out;
+}
